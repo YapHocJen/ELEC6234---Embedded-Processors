4438be976806ba8af7fafb41eacfcc5315f72569
YapHocJen <hocjenyap@gmail.com>
2025-08-26 20:38:56 +0800

    Add files via upload

---
+++ alucodes.sv
@@ -0,0 +1,10 @@
+//-----------------------------------------------------
+// File Name   : alucodes.sv
+// Function    : pMIPS ALU function code definitions 
+// Author:   tjk
+// Last rev. 23 Oct 12 
+//-----------------------------------------------------
+// 
+`define RADD  3'b010
+`define RSUB  3'b011  
+`define RMUL 3'b100
